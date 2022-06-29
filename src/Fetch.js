fc712332f3ad8cfc2587fc9b6dc2b151ecbcca6a
stephanieonyekaba <onyek009@umn.edu>
2022-06-29 01:31:06 -0400

    test scores added in toggle

--- src/Fetch.js
+++ src/Fetch.js
@@ -31,63 +31,32 @@ getRequest()
 
 
 
+
+
 const [isOpen, setIsOpen] = useState(false)
 
 
 
-let expandGrades = <div className="collapse">
-    <button className ="buttonCollapse" onClick={() => setIsOpen(!isOpen) }>
-
-    {
-    isOpen &&<span className="fa fa-minus" aria-hidden="true"> - </span>
-    }
-
-{!isOpen &&<span className="fa fa-plus" aria-hidden="true"> + </span>}
-
-
-
-    </button>
-
-    {isOpen &&<div className="expandedGrades"> hello</div>}
-
-</div>
-
-
+// let expandGrades = <div className="collapse">
+//                 <button className ="buttonCollapse" onClick={() => setIsOpen(!isOpen) }>
+            
+//                 {
+//                 isOpen &&<div className="button-sign" aria-hidden="true"> - </div>
+//                 }
+            
+//             {!isOpen &&<div className="button-sign" aria-hidden="true"> + </div>}
+            
+            
+            
+//                 </button>
+            
+//                 {isOpen &&<div className="expandedGrades"> p</div>}
+            
+//             </div>
 
 
 
-
-// const [showGradesList, setShowGradesList] = useState(false);
-
-// const toggleGradeList = () => {
-//     setShowGradesList(!showGradesList);
-//   }
-
-
-//   let gradeList = null;
-
-//   let buttonSign = null;
-
-//   if(showGradesList){
-
-//     buttonSign = <span className="fa fa-minus" aria-hidden="true"></span>
-//   } else {
-//     gradeList = null;
-//     buttonSign = <span className="fa fa-plus" aria-hidden="true"></span>
-//   } 
-
-
-
-
-
-
-
-
-
-
-
-
-
+const [showGradesList, setShowGradesList] = useState(false);
 
 
 
@@ -101,6 +70,7 @@ let expandGrades = <div className="collapse">
             else if (item.firstName.toLowerCase().includes(searchName.toLocaleLowerCase()) || item.lastName.toLowerCase().includes(searchName.toLocaleLowerCase())){
                 return item
             }
+
 
         }).map((item) => (
         
@@ -119,18 +89,33 @@ let expandGrades = <div className="collapse">
                                         <p>Company: {item.company}</p>
                                         <p>Skill: {item.skill}</p>
                                         <p>Average:  {item.grades.reduce((a,b) => a + parseInt(b), 0) / item.grades.length}%</p>
-                                        <p>{expandGrades}</p>
-
-
                                     
-                                        {/* <p className="Grade">Test 1:   {item.grades[0]}%</p>
-                                            <p className="Grade">Test 2:   {item.grades[1]}%</p>
-                                            <p className="Grade">Test 3:   {item.grades[2]}%</p>
-                                            <p className="Grade">Test 4:   {item.grades[3]}%</p>
+                                        <div className="text">
+                                        <button className ="buttonCollapse" onClick={() => setIsOpen(!isOpen) }>
+                                        {
+                                        isOpen &&<div className="button-sign" aria-hidden="true"> - </div>
+                                        }
+                                        {!isOpen &&<div className="button-sign" aria-hidden="true"> + </div>}
+                                        </button>
+                                        {isOpen &&<div className="test"> 
+                                        
+                                            <p>Test 1:   {item.grades[0]}%</p>
+                                            <p>Test 2:   {item.grades[1]}%</p>
+                                            <p>Test 3:   {item.grades[2]}%</p>
+                                            {/* <p >Test 4:   {item.grades[3]}%</p>
                                             <p className="Grade">Test 5:   {item.grades[4]}%</p>
                                             <p className="Grade">Test 6:   {item.grades[5]}%</p>
                                             <p className="Grade">Test 7:   {item.grades[6]}%</p>
-                                            <p className="Grade">Test 8:   {item.grades[7]}%</p> */}
+                                            <p className="Grade">Test 8:   {item.grades[7]}%</p>
+
+] */}
+                                        
+                                        </div>}
+                                        </div>
+                                    
+
+
+                                    
 
 
 
